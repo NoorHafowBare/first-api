a8d138dc89286c8fb9a953fef913fc7ffc0bfa5f
NoorHafowBare <noorhafowbare@gmail.com>
2025-06-26 18:32:13 +0300

    added post

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+
 app = FastAPI()
+
+class post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def root():
@@ -11,7 +20,8 @@ def get_users():
     return {"data": "this is your post"}
 
 @app.post("/createposts")
-def create_posts(payLoad: dict = Body(...)):
-    print(payLoad)
-    return {"new_post": f"title {payLoad['title']} content: {payLoad['content']}"}
+def create_posts(post: post):
+    print(post)
+    print(post.dict())
+    return {"data": post}
 
