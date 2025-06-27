de53469f3af242a9875848d6dff115b651ec5649
NoorHafowBare <noorhafowbare@gmail.com>
2025-06-27 12:55:21 +0300

    initial commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -12,17 +13,31 @@ class post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [{"title": "title of post1", "content": "content of post 1", "id": 1}, {"title": "favorite foods", "content": "i like pizza", "id": 2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
 @app.get("/")
 def root():
     return {"message": "Hello Welcome to Noor's Api!"}
 
 @app.get("/posts")
 def get_users():
-    return {"data": "this is your post"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(post: post):
-    print(post)
-    print(post.dict())
-    return {"data": post}
+    # print(post)
+    # print(post.dict())
+    post_dict = post.dict()
+    post_dict['id'] =  randrange(0, 100000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
 
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post =(id)
+    return {"post_detail": post}
